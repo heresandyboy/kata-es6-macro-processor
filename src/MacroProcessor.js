9b8759d6464bb94942dc9ae8bbef34f88c0291a5
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2018-07-10 13:26:40 +0100

    TEST: 'will replace "Happy celebration, name" with "Happy birthday Wilma"' - GREEN

--- src/MacroProcessor.js
+++ src/MacroProcessor.js
@@ -3,9 +3,19 @@ import TextBox from './macro.test'
 export default class MacroProcessor {
     constructor(textBox) {
         this.textBox = textBox ? textBox : new TextBox()
+        this.rules = {
+            'celebration': 'birthday',
+            'name': 'Wilma',
+        }
     }
 
     run() {
-        this.textBox.text = 'birthday'
+        this.replaceMacros(this.textBox)
+    }
+
+    replaceMacros(textBox) {
+        for (const search in this.rules) {
+            this.textBox.text = this.textBox.text.replace(new RegExp(search, 'g'), this.rules[search])
+        }
     }
 }
--- src/macro.test.js
+++ src/macro.test.js
@@ -10,10 +10,10 @@ describe('A macro processor', () => {
     })
 
     test('will replace "Happy celebration, name" with "Happy birthday Wilma"', () => {
-        let textBox = new TextBox('celebration')
+        let textBox = new TextBox('Happy celebration, name')
         let macro = new MacroProcessor(textBox).run()
 
-        expect(textBox.text).toEqual('Happy birthday Wilma')
+        expect(textBox.text).toEqual('Happy birthday, Wilma')
     })
 })
 
