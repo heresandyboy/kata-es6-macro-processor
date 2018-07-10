722869cbe5041196eeecfe5571807030bd0a0fa8
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2018-07-10 14:33:37 +0100

    REFACTOR: Simplified tests and Added a second observer

--- src/macro.test.js
+++ src/macro.test.js
@@ -1,34 +1,43 @@
 import MacroProcessor from './MacroProcessor'
 
 
-describe('A macro processor', () => {
-    test('will replace "celebration" with "birthday"', () => {
-        let textBox = new TextBox()
-        let macro = new MacroProcessor()
+let _macro = undefined
+let _textBox = undefined
 
-        macro.Attach(textBox)
-        macro.replace('celebration')
-        expect(textBox.text).toEqual('birthday')
+describe('A macro processor', () => {
+
+    beforeEach(() => {
+        _textBox = new TextBox()
+        _macro = new MacroProcessor()
+    })
+
+    test('will replace "celebration" with "birthday"', () => {
+
+        _macro.Attach(_textBox)
+        _macro.replace('celebration')
+
+        expect(_textBox.text).toEqual('birthday')
     })
 
     test('will replace "Happy celebration, name" with "Happy birthday Wilma"', () => {
-        let textBox = new TextBox()
-        let macro = new MacroProcessor()
 
-        macro.Attach(textBox)
-        macro.replace('Happy celebration, name')
+        _macro.Attach(_textBox)
+        _macro.replace('Happy celebration, name')
 
-        expect(textBox.text).toEqual('Happy birthday, Wilma')
+        expect(_textBox.text).toEqual('Happy birthday, Wilma')
     })
 
-    test('updates the observer with the results of the subjects replace function', () => {
-        var observer = new TextBox()
-        var subject = new MacroProcessor()
+    test('updates the observers with the results of the subjects replace function', () => {
+        const observer = new TextBox()
+        const observer2 = new TextArea()
+        const subject = new MacroProcessor()
 
         subject.Attach(observer)
+        subject.Attach(observer2)
         subject.replace('Happy celebration, name')
 
         expect(observer.text).toEqual('Happy birthday, Wilma')
+        expect(observer2.text).toEqual('Happy birthday, Wilma')
     })
 })
 
@@ -39,6 +48,17 @@ class Text {
 }
 
 export class TextBox extends Text {
+    constructor(text) {
+        super()
+        this.text = text
+    }
+
+    Update(text) {
+        this.text = text
+    }
+}
+
+export class TextArea extends Text {
     constructor(text) {
         super()
         this.text = text
