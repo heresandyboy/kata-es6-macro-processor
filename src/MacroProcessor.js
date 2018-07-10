ea6d1ff4f8e4bb3368e742872e16cee161f931a9
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2018-07-10 14:26:51 +0100

    REFACTOR to Observer Pattern (I think!?)

--- src/MacroProcessor.js
+++ src/MacroProcessor.js
@@ -1,21 +1,44 @@
 import TextBox from './macro.test'
 
-export default class MacroProcessor {
-    constructor(textBox) {
-        this.textBox = textBox ? textBox : new TextBox()
+
+class Processor {
+    constructor() {}
+
+    Attach(Observer) {
+        this.observers.push(Observer);
+        console.log('Observer attached')
+    }
+
+    Dettach(Observer) {
+        for (var i in this.observers)
+            if (this.observers[i] === Observer)
+                this.observers.splice(i, 1)
+    }
+
+    Notify(text) {
+        console.log('Subject Notify')
+        for (var i in this.observers) {
+            this.observers[i].Update(text);
+        }
+    }
+}
+
+export default class MacroProcessor extends Processor {
+    constructor() {
+        super()
+        this.observers = []
+        this.subjectState = null
         this.rules = {
             'celebration': 'birthday',
             'name': 'Wilma',
         }
+        console.log('ConcreteSubject created')
     }
 
-    run() {
-        this.replaceMacros(this.textBox)
-    }
-
-    replaceMacros(textBox) {
+    replace(text) {
         for (const search in this.rules) {
-            this.textBox.text = this.textBox.text.replace(new RegExp(search, 'g'), this.rules[search])
+            text = text.replace(new RegExp(search, 'g'), this.rules[search])
         }
+        this.Notify(text)
     }
 }
--- src/macro.test.js
+++ src/macro.test.js
@@ -3,22 +3,48 @@ import MacroProcessor from './MacroProcessor'
 
 describe('A macro processor', () => {
     test('will replace "celebration" with "birthday"', () => {
-        let textBox = new TextBox('celebration')
-        let macro = new MacroProcessor(textBox).run()
+        let textBox = new TextBox()
+        let macro = new MacroProcessor()
 
+        macro.Attach(textBox)
+        macro.replace('celebration')
         expect(textBox.text).toEqual('birthday')
     })
 
     test('will replace "Happy celebration, name" with "Happy birthday Wilma"', () => {
-        let textBox = new TextBox('Happy celebration, name')
-        let macro = new MacroProcessor(textBox).run()
+        let textBox = new TextBox()
+        let macro = new MacroProcessor()
+
+        macro.Attach(textBox)
+        macro.replace('Happy celebration, name')
 
         expect(textBox.text).toEqual('Happy birthday, Wilma')
     })
+
+    test('updates the observer with the results of the subjects replace function', () => {
+        var observer = new TextBox()
+        var subject = new MacroProcessor()
+
+        subject.Attach(observer)
+        subject.replace('Happy celebration, name')
+
+        expect(observer.text).toEqual('Happy birthday, Wilma')
+    })
 })
 
-export class TextBox {
+class Text {
+    constructor() {}
+
+    Update() {}
+}
+
+export class TextBox extends Text {
     constructor(text) {
+        super()
+        this.text = text
+    }
+
+    Update(text) {
         this.text = text
     }
 }
